12f2761410c48157b567b0bf09bba51e9daf65d7
faqeel <contact@fares.ws>
2022-11-23 18:28:18 +0300

    Setup Vue.js 3
    Install and configure Vue.js 3 and Vue Router. Add base css and js resources. Rename the welcome.blade.php view to be index.blade.php.

---
+++ resources/js/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="content">
+		<h1>Hey! 👋🏻</h1>
+		<p><code>Vue.js</code> has been set up successfully.</p>
+		<p>Let's build something awesome! 🚀</p>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.content {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+}
+
+h1 {
+	font-size: 2rem;
+	font-weight: 500;
+	margin: 2rem 0;
+}
+
+p {
+	font-size: 1rem;
+	margin: 0.25rem 0;
+}
+
+code {
+	background: var(--vt-c-bg-mute);
+	color: var(--vt-c-text-code);
+	padding: 0.2em 0.4em;
+	margin: 0;
+	font-size: 85%;
+	border-radius: 6px;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        hmr: {
+            /**
+             * Handle the issue of resources failing to load due to
+             * an invalid address when using Vite development server
+             * within Windows (WSL2).
+             * 
+             * @see https://github.com/laravel/vite-plugin/issues/28
+             */
+            host: 'localhost'
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/main.js'],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
