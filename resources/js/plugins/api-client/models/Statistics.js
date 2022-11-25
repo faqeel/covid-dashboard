ed5f9d10c54ba15b74b997c352b2955d46f02c81
faqeel <contact@fares.ws>
2022-11-25 04:35:48 +0300

    (Statistics.js): Handle a country with no statistics

--- resources/js/plugins/api-client/models/Statistics.js
+++ resources/js/plugins/api-client/models/Statistics.js
@@ -3,13 +3,17 @@ import { isEmpty } from "../Utils";
 export default class Statistics {
 
 	constructor(response) {
-		const UNDEFINED_VALUE_MESSAGE = '-';
-		this.confirmed = isEmpty(response.confirmed) || response.confirmed == -1 ?
-			UNDEFINED_VALUE_MESSAGE : response.confirmed;
-		this.deaths = isEmpty(response.deaths) || response.deaths == -1 ?
-			UNDEFINED_VALUE_MESSAGE : response.deaths;
-		this.recovered = isEmpty(response.recovered) || response.recovered == -1 ?
-			UNDEFINED_VALUE_MESSAGE : response.recovered;
+		const UNDEFINED_VALUE = '-';
+		if (response) {
+			this.confirmed = isEmpty(response.confirmed) || response.confirmed == -1 ?
+				UNDEFINED_VALUE : response.confirmed;
+			this.deaths = isEmpty(response.deaths) || response.deaths == -1 ?
+				UNDEFINED_VALUE : response.deaths;
+			this.recovered = isEmpty(response.recovered) || response.recovered == -1 ?
+				UNDEFINED_VALUE : response.recovered;
+		} else {
+			this.confirmed = this.deaths = this.recovered = UNDEFINED_VALUE;
+		}
 	}
 
 }
