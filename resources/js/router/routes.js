62f650b0558e4859775f50e8ce2688f05babab40
faqeel <contact@fares.ws>
2022-11-26 14:56:35 +0300

    feat: Create page not found view

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,9 +1,11 @@
 import GlobalViewRouter from "./GlobalViewRouter";
 import CountriesViewRouter from "./CountriesViewRouter";
+import PageNotFoundViewRouter from "./PageNotFoundViewRouter";
 
 const routes = [
 	GlobalViewRouter,
 	CountriesViewRouter,
+	PageNotFoundViewRouter,
 ];
 
 export default routes;
